2a6382880a977f5d08389e9094e19b198b25aa05
shamsher singh <shamshersingh8574@gmail.com>
2024-09-05 13:21:53 +0530

    dome

--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,16 +1,30 @@
+"use client"
+import axios from 'axios';
+import React, { useState } from 'react';
 
 
-const Example = ({ringDetail}) => {
-    console.log(ringDetail);
-    
-
+const Example = () => {
+const[state, setState] = useState()
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+    .then((res)=>{
+        console.log(res.data);
+        setState(res.data)
+    })
+    .catch((error)=>{
+        console.error(error);
+    })
   return (
     <div>
       <h1>Example</h1>
-      
+      <ul>
+        {state?.map((item)=>{
+            return (<>
+            <p>{item?.title}</p>
+            </>)
+        })}
+      </ul>
     </div>
   );
 };
 
 export default Example;
-
