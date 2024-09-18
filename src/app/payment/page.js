74799a897bf8b560a2535ed816074e0311acf921
shamsher singh <shamshersingh8574@gmail.com>
2024-09-18 17:55:08 +0530

    18-9

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,77 +1,9 @@
-import { Suspense } from "react";
-import LoaderSpinner from "../_componentStatic/LoaderSpinner";
-import PaymentForm from "./PaymentForm";
+import React from 'react'
 
-const fetchPaymentServer = async () => {
-  let payment = [];
-  try {
-    const response = await fetch(
-      `${process.env.BASE_URL}/cms-metadata?route=payment`
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    payment = await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-  return payment;
-};
-
-export async function generateMetadata() {
-  const data = await fetchPaymentServer();
-
-  if (data) {
-    const metadata = {
-      title: data.data.meta_title || "Default Title",
-      description: data.data.meta_description || "Default Description",
-      openGraph: {
-        title: data.data.meta_title || "Default Title",
-        description: data.data.meta_description || "Default Description",
-        url: data.data.meta_url || "http://default-url.com",
-        siteName: data.data.meta_site_name || "Default Site Name",
-        images: [
-          {
-            url: data.data.meta_image_url || "https://assets.rocksama.com/public/storage/images/1716284040_SAMA.png",
-            width: 800,
-            height: 600,
-            alt: data.data.meta_image_alt || "Default Image Alt",
-          },
-        ],
-      },
-    };
-
-    return metadata;
-  }
-
-  return {
-    title: "Default Title",
-    description: "Default Description",
-    openGraph: {
-      title: "Default Title",
-      description: "Default Description",
-      url: "http://default-url.com",
-      siteName: "Default Site Name",
-      images: [
-        {
-          url: "http://default-image-url.com",
-          width: 800,
-          height: 600,
-          alt: "Default Image Alt",
-        },
-      ],
-    },
-  };
-}
-const PaymentServer = async () => {
-  const payment = await fetchPaymentServer();
+ const PaymentServer = () => {
   return (
-    <>
-    <Suspense fallback={<LoaderSpinner/>}>
-      <PaymentForm payment={payment} />
-    </Suspense>
-    </>
-  );
-};
+    <div>page</div>
+  )
+}
 
 export default PaymentServer;
