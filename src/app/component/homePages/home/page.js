f89f48b6812e40f582d8f33f65e6ef7ec1b3a027
shamsher singh <shamshersingh8574@gmail.com>
2024-09-18 18:13:08 +0530

    18-9

--- src/app/component/homePages/home/page.js
+++ src/app/component/homePages/home/page.js
@@ -229,19 +229,19 @@ export default async function HomePageData() {
         {/* <pre>{JSON.stringify(homeDetail.data, null, 2)}</pre> */}
         <div className="home-page">
         <Banner homeContext={homeContext}/>
-        {/* <ShopDiamondShape shapeData={shapeData.data} />
-        <SeeProducts anniversaryRings={anniversaryRings.data} diamondPendants={diamondPendants.data} tennisBracelets={tennisBracelets.data} diamondStuds={diamondStuds.data}/>
+        <ShopDiamondShape shapeData={shapeData.data} />
+        {/* <SeeProducts anniversaryRings={anniversaryRings.data} diamondPendants={diamondPendants.data} tennisBracelets={tennisBracelets.data} diamondStuds={diamondStuds.data}/> */}
         <ShopDiamondCotegory shopStyle={shopStyle.data}/>
         <BridalJewellery home={homeAllSections}/>
         <AnniversaryRings home={homeAllSections.data}/>
         <CelebarteLove home={homeAllSections}/>
-        <WeddingCollection engagementRingMain={engagementRingMain.data} weddingJewelry={weddingJewelry.data} weddingCollection={weddingCollection.data}/>
+        {/* <WeddingCollection engagementRingMain={engagementRingMain.data} weddingJewelry={weddingJewelry.data} weddingCollection={weddingCollection.data}/> */}
 
         <EngagementBridal home={homeAllSections}/>
 
         <LoveBrilliance home={homeAllSections}/>
         
-        <AnniversaryRingFeatured home={homeAllSections}/> */}
+        <AnniversaryRingFeatured home={homeAllSections}/>
       </div>
       </>
     );
