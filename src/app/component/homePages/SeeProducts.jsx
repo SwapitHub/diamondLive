b8c7191f0f8c6207e1fb6383d10e7933ed61fb5e
shamsher singh <shamshersingh8574@gmail.com>
2024-09-23 17:44:32 +0530

    done

--- src/app/component/homePages/SeeProducts.jsx
+++ src/app/component/homePages/SeeProducts.jsx
@@ -1,64 +1,7 @@
-"use client";
-import Link from "next/link";
-import React from "react";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import React from 'react'
 
-export const SeeProducts = ({
-  anniversaryRings,
-  diamondPendants,
-  tennisBracelets,
-  diamondStuds,
-}) => {
+export const SeeProducts = () => {
   return (
-    <section className="see-products-main">
-      <div className="container">
-        <div className="common-row see-products">          
-          <div className="see-products-img">
-            <Link href={anniversaryRings?.url} className="See-Products">
-              <span>Trellis Rings</span>
-              <span> See Products <MdOutlineKeyboardArrowRight /> </span>
-            </Link>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: anniversaryRings?.description,
-              }}
-            ></div>
-          </div>
-          <div className="see-products-img">
-            <Link href={diamondPendants?.url} className="See-Products">
-              <span>Vintage Rings</span>
-              <span> See Products <MdOutlineKeyboardArrowRight /> </span>
-            </Link>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: diamondPendants?.description,
-              }}
-            ></div>
-          </div>
-          <div className="see-products-img">
-            <Link href={tennisBracelets?.url} className="See-Products">
-              <span>Eternity Rings</span>
-              <span> See Products <MdOutlineKeyboardArrowRight /> </span>
-            </Link>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: tennisBracelets?.description,
-              }}
-            ></div>
-          </div>
-          <div className="see-products-img">
-            <Link href={diamondStuds?.url} className="See-Products">
-              <span>Nature Inspired Rings</span>
-              <span> See Products <MdOutlineKeyboardArrowRight /> </span>
-            </Link>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: diamondStuds?.description,
-              }}
-            ></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+    <div>SeeProducts</div>
+  )
+}
