1a0cb7cf3f08bbc196e14fecc727d03c1000333a
shamsher singh <shamshersingh8574@gmail.com>
2024-09-24 17:42:12 +0530

    24-09

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export const runtime = 'nodejs'; // You can specify either 'edge' or 'nodejs'
-export const sizeLimit = '10mb'; // Set size limit to 10MB
+export const sizeLimit = '50mb'; // Set size limit to 10MB
 
 export async function POST(req) {
   const body = await req.json();
