235e6454eb68380d52358e8887c2b4cf80dcdc01
shamsher singh <shamshersingh8574@gmail.com>
2024-10-03 15:44:10 +0530

    03-10

--- src/app/_componentStatic/LoaderSpinner.jsx
+++ src/app/_componentStatic/LoaderSpinner.jsx
@@ -1,14 +1,11 @@
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
 
 function LoaderSpinner() {
-const {imgAssetsUrl} = useContext(UserContext)
   return (
     <div className="sweet-loading">
        <div className="loader">
       <object
         type="image/svg+xml"
-        data={`${imgAssetsUrl}/public/Animated-Loader.svg`}
+        data={`https://assets.rocksama.com/public/Animated-Loader.svg`}
         width="100"
         height="100"
       >
